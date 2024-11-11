c949e4e467381bcce5149e7fabed26da14b1ff1e
parsival2369 <167647797+parsival2369@users.noreply.github.com>
2024-11-10 20:14:18 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioAbstraccion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAbstraccion
+{
+    internal class Program
+    {
+        static void Main(string[] args)      
+        {
+                   }
+    }
+}
